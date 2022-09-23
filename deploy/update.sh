f338eaf328b651aa0b5c4cbb932fb13681d67e5d
Yash <yash8135512@gmail.com>
2022-09-24 02:50:59 +0530

    Update python to python3

--- deploy/update.sh
+++ deploy/update.sh
@@ -5,8 +5,8 @@ set -e
 PROJECT_BASE_PATH='/usr/local/apps/profiles-rest-api'
 
 git pull
-$PROJECT_BASE_PATH/env/bin/python manage.py migrate
-$PROJECT_BASE_PATH/env/bin/python manage.py collectstatic --noinput
+$PROJECT_BASE_PATH/env/bin/python manage.py3 migrate
+$PROJECT_BASE_PATH/env/bin/python manage.py3 collectstatic --noinput
 supervisorctl restart profiles_api
 
 echo "DONE! :)✅👍"
