c837e2b867dd4216ba96787b36c62591d7a19d41
Yash <yash8135512@gmail.com>
2022-09-23 19:24:41 +0530

    Added profiles viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 
 urlpatterns = [
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
+from rest_framework.authentication import TokenAuthentication
 
-from profiles_api import serializers
+
+from profiles_api import permissions
+from profiles_api import serializers, models
 
 
 class HelloApiView(APIView):
@@ -63,7 +66,7 @@ class HelloViewSet(viewsets.ViewSet):
 
     def create(self, request):
         """ Create a new hello messaege """
-        serializer = self.serializer_class(data = request.data)
+        serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
             name = serializer.validated_data.get('name')
@@ -74,7 +77,7 @@ class HelloViewSet(viewsets.ViewSet):
                 serializer.errors,
                 status=status.HTTP_400_BAD_REQUEST
             )
-    
+
     def retrieve(self, request, pk=None):
         """ Handle getting an object by its ID """
 
@@ -94,3 +97,11 @@ class HelloViewSet(viewsets.ViewSet):
         """ Handle removing an object """
 
         return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating, creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
